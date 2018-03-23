78883b6c43b2d6d08a7ce2876151ce56a2cd4c3d
Carlos Penaranda <carlos.penaranda74@gmail.com>
2018-03-23 11:57:52 +0100

    first_sys_config: added ssh-keygen

--- first_sys_config.sh
+++ first_sys_config.sh
@@ -1,11 +1,36 @@
 #!/usr/bin/env bash
 
+# Dont continue when script fail
+set -e
+
+# Dont be root
+if [ $(id -u) -eq 0 ]; then
+   echo Dont run this script as root
+   echo But as a sudoer user
+   exit 1
+fi
+
 # Required package
 packages="git ansible"
 if [ -e /etc/debian-release ]; then
    sudo apt update -y && apt install -y $packages
 elif [ -e /etc/arch-release ]; then
    sudo pacman -S --needed $packages
+fi
+
+# Required ssh key
+if ! [ -f ~/.ssh/id_rsa.pub ];
+   echo A ssh key is needed to deploy on github
+   echo and been able to clone cpenar/ansible repo
+   echo
+
+   ssh-keygen -t rsa 4096
+
+   echo
+   echo Please export ~/.ssh/id_rsa.pub to your github acount
+   echo before reruning this script
+
+   exit 0
 fi
 
 git clone git@github.com:cpenar/ansible.git ~/work/ansible
